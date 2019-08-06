e0d4e48540fa08a8fd0e09c1c7d26acde5e8e506
Andreas Susewind <andreas@amsusewind.de>
2019-08-06 23:03:40 +0200

    first

---
+++ railroad/src/railroad/Player.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * 2019, All rights reserved.
+ *******************************************************************************/
+package railroad;
+
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of Player.
+ * 
+ * @author andreas
+ */
+public class Player {
+	// Start of user code (user defined attributes for Player)
+
+	// End of user code
+
+	/**
+	 * The constructor.
+	 */
+	public Player() {
+		// Start of user code constructor for Player)
+		super();
+		// End of user code
+	}
+
+	// Start of user code (user defined methods for Player)
+
+	// End of user code
+
+}
